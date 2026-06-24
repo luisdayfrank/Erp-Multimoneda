6b9db697e476b943388d510446ee751faae7ba9e
luisdayfrank <60307322+luisdayfrank@users.noreply.github.com>
2026-06-24 06:59:28 -0400

    Update Django settings with new configurations

--- erp-backend/config/settings.py
+++ erp-backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8%!$#ua2h6)3w1ioaz^(slvk%b)7$(sp#_wpm&%e9q!th-bmn(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition

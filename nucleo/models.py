6c2d09a03a06ab8ccf7bd834010ac5918fb4f1a3
luisdayfrank <60307322+luisdayfrank@users.noreply.github.com>
2026-08-10 18:46:37 -0400

    Update models.py

--- nucleo/models.py
+++ nucleo/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from decimal import Decimal
